da111d07ad8636db634be1fbc29ff7b5f224961e
Helen Kli <klitsunova2000@mail.ru>
2021-11-06 17:38:50 +0300

    Resizing the table

--- code.cpp
+++ code.cpp
@@ -1,40 +1,41 @@
-#include <iostream>
 #include <array>
-#include <cstdlib>
 #include <cmath>
+#include <cstdlib>
+#include <iostream>
 #include <unistd.h>
 #include <vector>
 
+const int length = 32;
+const int hight = 11; 
+const int tabl_size = length * hight;
 
-std::array<int,64> null_array() { 
-    std::array<int,64> null;
-    for (int i = 0; i < 64; i++) {
+std::array<int,tabl_size> null_array() { 
+    std::array<int,tabl_size> null;
+    for (int i = 0; i < tabl_size; i++) {
         null[i] = 0;
     }
     return null;
 }
 
-
 struct ListNode {
-    std::array<int,64> val;
+    std::array<int,tabl_size> val;
     ListNode* next;
     ListNode(): val(null_array()), next(nullptr) {}
-    ListNode(std::array<int,64> x): val(x), next(nullptr) {}
-    ListNode(std::array<int,64> x, ListNode* n): val(x), next(n) {}
+    ListNode(std::array<int,tabl_size> x): val(x), next(nullptr) {}
+    ListNode(std::array<int,tabl_size> x, ListNode* n): val(x), next(n) {}
 };
 
-std::array<int, 64> step(std::array<int, 64> arr) {
-    std::array<int, arr.size()> res; 
-    int s = sqrt(arr.size());
-    for (int i = 0; i < s; i++) {
-        for (int j = 0; j < s; j++) {
+std::array<int,tabl_size> step(std::array<int,tabl_size> arr) {
+    std::array<int, tabl_size> res; 
+    for (int i = 0; i < hight; i++) {
+        for (int j = 0; j < length; j++) {
             int count = 0;
             int k = 0, n = 0;
             int l = 3, m = 3;
             if (i == 0) {
                 k = 1;
             }
-            if (i == s - 1) {
+            if (i == hight - 1) {
                 l = 2;
             }
             for (k; k < l; k++) {
@@ -43,38 +44,37 @@ std::array<int, 64> step(std::array<int, 64> arr) {
                 } else {
                     n = 0;
                 }
-                if (j == s - 1) {
+                if (j == length - 1) {
                     m = 2;
                 } else {
                     m = 3;
                 }
                 for (n; n < m; n++) {
-                    if (arr[s * (i + k - 1) + j + n - 1] == 1) {
+                    if (arr[length * (i + k - 1) + j + n - 1] == 1) {
                         count++;
                     }
                 }
             }
-            if (arr[s * i + j] == 0 && count == 3){
-                res[s * i + j] = 1;
-            } else if (arr[s * i + j] == 1 && (count == 3 || count == 4)){
-                res [s * i + j] = 1;
+            if (arr[length * i + j] == 0 && count == 3){
+                res[length * i + j] = 1;
+            } else if (arr[length * i + j] == 1 && (count == 3 || count == 4)){
+                res [length * i + j] = 1;
             } else {
-                res[s * i + j] = 0;
+                res[length * i + j] = 0;
             }
         }
     }
     return res; 
 }
 
-bool alive(std::array<int,64> arr) {
+bool alive(std::array<int,tabl_size> arr) {
     if (arr != null_array()) {
         return true;
     }
     return false;
 }
 
-
-bool not_period(std::array<int,64> arr, ListNode* mass){
+bool no_period(std::array<int,tabl_size> arr, ListNode* mass){
     while (mass != nullptr) {
         if (mass->val == arr) {
             return false;
@@ -84,21 +84,20 @@ bool not_period(std::array<int,64> arr, ListNode* mass){
     return true;
 }
 
-void print(std::array<int,64> arr) {
-    int MeshSize = sqrt(arr.size());
+void print(std::array<int,tabl_size> arr) {
     std::cout << std::endl;
-    for (int i = 0; i < MeshSize; i++){
+    for (int i = 0; i < length; i++) {
             std::cout << " ___";
         }
-    for (int j = 0; j < MeshSize; j++) {
+    for (int j = 0; j < hight; j++) {
         std::cout << std::endl;
-        for (int i = 0; i < MeshSize; i++) {
+        for (int i = 0; i < length; i++) {
             std::cout << "|   ";
         }
         std::cout << "|" << std::endl << "|";
-        for (int i = 0; i < MeshSize;i++) {
+        for (int i = 0; i < length; i++) {
             std::cout << " ";
-            if (arr[MeshSize * j + i] == 1) {
+            if (arr[length * j + i] == 1) {
                 std::cout << "*";
             } else {
                 std::cout << " ";
@@ -107,7 +106,7 @@ void print(std::array<int,64> arr) {
             std::cout << "|";
         }
         std::cout << std::endl;
-        for (int i = 0; i < MeshSize; i++) {
+        for (int i = 0; i < length; i++) {
             std::cout << "|___";
         }
         std::cout << "|";
@@ -115,27 +114,20 @@ void print(std::array<int,64> arr) {
     std::cout << std::endl << std::endl;
 }
 
-ListNode* add_array(std::array<int,64> arr, ListNode* head) {
+ListNode* add_array(std::array<int,tabl_size> arr, ListNode* head) {
     ListNode* result = new ListNode(arr, head);
     return result;
 }
 
 
 int main() {
-    const int size = 8;
     ListNode* head = nullptr;
-    std::array<int,9> a = {1,2,4,0,1,3,4,5,1};
-    std::array<int,9> b = {0,1,1,2,2,4,4,4,5};
-    std::array <int, size * size> start = null_array();
-    for (int i = 0; i < 9; i++){
-        start[size * b[i] + a[i]] = 1;
-    }
-    /*for (int i = 0; i < size * size; i++) {
+    std::array <int, tabl_size> start = null_array();
+    for (int i = 0; i < tabl_size; i++) {
         start[i] = std::rand() % 2;
-    }*/
-    print(start);
-    while (alive(start) && not_period(start, head)) {
-        sleep(2);
+    }
+    while (alive(start) && no_period(start, head)) {
+        sleep(1);
         std::system("clear");
         head = add_array(start, head);
         start = step(start); 
